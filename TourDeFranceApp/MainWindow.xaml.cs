80c9629b352b78466ff13f19467110f6a16974bf
MattyxD19 <mathiasbc97@gmail.com>
2019-09-27 12:21:39 +0200

    new push

--- TourDeFranceApp/MainWindow.xaml.cs
+++ TourDeFranceApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TourDeFranceApp.Model;
 
 namespace TourDeFranceApp
 {
@@ -23,7 +24,7 @@ namespace TourDeFranceApp
         public MainWindow()
         {
             InitializeComponent();
-            foreach (var cyclist in Model.Parser.CyclistListMaker())
+            foreach (var cyclist in Parser.CyclistListMaker())
         	{
                 listViewName.Items.Add(cyclist);
 	        }
@@ -32,22 +33,59 @@ namespace TourDeFranceApp
 
         private void OrderRank_Selected(object sender, RoutedEventArgs e)
         {
+            listViewName.Items.Clear();
+            List<Cyclist> SortedList = Parser.CyclistListMaker().OrderBy(o => o.EndPosition).ToList();
+            var top10 = SortedList.Take(10);
 
+            foreach (var top in top10)
+            {
+                listViewName.Items.Add(top);
+            }
         }
 
         private void OrderName_Selected(object sender, RoutedEventArgs e)
         {
+            listViewName.Items.Clear();
+            List<Cyclist> sortedList = Parser.CyclistListMaker().OrderBy(o => o.Name).ToList();
 
+            foreach (var name in sortedList)
+            {
+                listViewName.Items.Add(name);
+            }
         }
 
         private void OrderCountry_Selected(object sender, RoutedEventArgs e)
         {
+            listViewName.Items.Clear();
+            List<Cyclist> SortedList = Parser.CyclistListMaker().OrderBy(o => o.Country).ToList();
+            var top10 = SortedList.Take(10);
 
+            foreach (var top in top10)
+            {
+                listViewName.Items.Add(top);
+            }
         }
 
         private void OrderResultTime_Selected(object sender, RoutedEventArgs e)
         {
 
+            listViewName.Items.Clear();
+            List<Cyclist> SortedList = Parser.CyclistListMaker().OrderBy(o => o.ResultTime).ToList();
+            var top10 = SortedList.Take(10);
+
+            foreach (var top in top10)
+            {
+                listViewName.Items.Add(top);
+            }
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Cyclist findCyclist = new Cyclist();
+            //string searchText = SearchBar.Text;
+            //Convert.ChangeType(searchText, typeof(Object));
+            //listViewName.Items.Clear();
+            //listViewName.Items.Add(Parser.CyclistListMaker().Contains(searchText));
         }
     }
 }
